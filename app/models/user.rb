9f084b7773282746966a5e7676bbfa3773fcd3ff
koma.san <komachi@plainwood.com>
2021-08-14 15:12:19 +0000

    firstcommit

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ApplicationRecord
+    before_save{ self.email.downcase! }
+    validates :name, presence: true, length:{ maximum: 50 }
+    validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
+                    uniqueness: { case_sensitive: false }
+
+    
+    has_secure_password
+    #users テーブルにパスワードを保存するとき、パスワードを暗号化して保存する。
+    #ログインフォーム用に password と password_confirmation という変数をモデルに追加して、ログイン認証時のパスワード確認のために使用する。
+    #password と password_confirmation には平文のパスワードが代入されるが、users テーブルのカラムではない一時的な変数として扱われるので、保存はされない。ログイン認証用のメソッド authenticate を提供する
+end
